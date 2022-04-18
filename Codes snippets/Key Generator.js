62d7c80544bd67e7fd1d01a410edc00644765878
Naflouille <69418024+W0lfan@users.noreply.github.com>
2022-04-18 15:37:57 +0200

    Not working, WIP.

--- Codes snippets/Key Generator.js
+++ Codes snippets/Key Generator.js
@@ -1,10 +1,21 @@
 
 var keys = {};
+var Players_Playing = [[],[]];
 var Waiting_Players = [];
-var Generate_Key = function(New = true, Player_Id, length = 11) {
+const Max = 2;
+
+var Return_Keys_Values = function(value, fromParameter, optionnal) {
+    for (let key in keys) {
+        if (keys[key][fromParameter] === optionnal) {
+            return keys[key][value];
+        }
+    }
+};
+
+var Generate_Key = function(New = true, Player_Id, length = 11, team, name) {
     if (New) {
         Object.assign(keys, {
-            [Object.keys(keys).length]  : {Id: Player_Id,Ship_Key : []}
+            [Object.keys(keys).length]  : {Id: Player_Id,Ship_Key : [],Team: team, Name: name}
         })
         let alphabet = [
             "a","b","c","d","e","f","g","h","i","j","k","l","n","m","o","p","q","r","s","t","u","v","w","x","y","z",
@@ -51,7 +62,7 @@ var Simulate_On_Click = function(click, playerId) {
                 Key = keys[key].Ship_Key;
             }
         }
-        if (!keyIn) Key = Generate_Key(true,playerId,11);
+        if (!keyIn) Key = Generate_Key(true,playerId,11, "test");
         Waiting_Players.push(Key);
     } else if (!click) {
         for (let key in keys) {
@@ -65,13 +76,51 @@ var Simulate_On_Click = function(click, playerId) {
         }
     }
 }
-Simulate_On_Click(true, 0)
-Simulate_On_Click(true, 1)
-Simulate_On_Click(true, 2)
-Simulate_On_Click(false,1)
-Simulate_On_Click(true, 0)
 
-console.log(keys, Waiting_Players)
+var Waiting_Filter = function(id) {
+    let Key = Return_Keys_Values("Ship_Key","Id",id);
+    let Team = Return_Keys_Values("Team","Id",id);
+    if (Players_Playing[Team].length < Max/2 && !Players_Playing[Team].join('').includes(Key)) {
+        //console.log('Allowed ship!\nShip with custom key "',Key,'" allowed to play on team ', Team);
+        Players_Playing[Team].push(Key);
+        Waiting_Players.splice(Waiting_Players.indexOf(Key),1);
+    }
+};
+// Ship Init : 3 ships enters the game
+Generate_Key(true,0,11,0, "DSE"); // Team 0
+Generate_Key(true,1,11,1, "Glitch"); // Team 1
+Generate_Key(true,2,11,0, "Wolfan"); // Team 0
+// 3 Ships choose to wait:
+Simulate_On_Click(true,1);
+Simulate_On_Click(true,0);
+Simulate_On_Click(true,2);
+
+console.log(keys, "Ships waiting custom IDs: ", Waiting_Players.join(', '));
+var echo = function(stuff) {console.log(stuff);}
+// If enough players waiting:
+Waiting_Players.forEach(id => {
+    console.log(Return_Keys_Values("Name","Ship_Key",id))
+})
+//if (Waiting_Players.length >= Max) {
+    Waiting_Players.forEach(key => {
+        let ID = Return_Keys_Values("Id","Ship_Key",key);
+        let Team = Return_Keys_Values("Team","Ship_Key",key);
+        let Name = Return_Keys_Values("Name","Ship_Key",key);
+        echo(Name)
+        if (Players_Playing[Team].length < Max/2 ) {
+            Waiting_Filter(ID);
+        }
+    })
+//}
+for (let customKey in Players_Playing) {
+    let Name = Return_Keys_Values("Name","Ship_Key",Players_Playing[customKey][0]);
+    let Team = Return_Keys_Values("Team","Ship_Key",Players_Playing[customKey][0]);
+    console.log(
+        "Players allowed (IDs) to play:\n",
+        Name, '<- Team ', Team, "\n"
+    )
+}
+
 
 
 
